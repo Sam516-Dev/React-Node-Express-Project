84bda336c8396684248f95315697534adabaa6b8
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-09-19 23:49:24 +0300

    compiled successiful
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

---
+++ server.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/api', (req, res) =>
+//   res.json({ users: ['userOne', 'userTwo', 'userThree'] }),
+// )
+
+// app.listen(5000, () => {
+//   console.log('Application running on port 5000')
+// })
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
