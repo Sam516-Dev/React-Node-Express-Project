b2838a5ce440955107dbea63e98a6a1dc99b95cc
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-09-20 00:41:28 +0300

    corrected
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-// const express = require('express')
-
-// const app = express()
-
-// app.get('/api', (req, res) =>
-//   res.json({ users: ['userOne', 'userTwo', 'userThree'] }),
-// )
-
-// app.listen(5000, () => {
-//   console.log('Application running on port 5000')
-// })
-
 const express = require('express')
+
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) =>
+  res.json({ users: ['userOne', 'userTwo', 'userThree'] }),
+)
+
+app.listen(5000, () => {
+  console.log('Application running on port 5000')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
