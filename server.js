9bb3b59c5cd34288f0b89986c937c6e418b875f0
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-09-20 23:55:41 +0300

    api added
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/apianimals', (req, res) =>
   res.json({ books: ['dog', 'cat', 'rabbit'] }),
 )
 
+app.get('/apifruits', (req, res) =>
+  res.json({ books: ['melon', 'apple', 'mango'] }),
+)
+
 app.listen(5000, () => {
   console.log('Application running on port 5000')
 })
