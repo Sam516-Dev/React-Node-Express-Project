ad1169fb3f2ee1c01bcb9e83068a42371a0dd5ee
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-09-25 23:11:44 +0300

    last today
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,7 +4,7 @@ import { Title, Body } from './styled-components/styles'
 function App() {
   return (
     <div>
-      <Title> Hello Sam </Title>
+      <Title> Hello sam </Title>
       <Body> this is a react application </Body>
     </div>
   )
