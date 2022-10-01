66d7b1c979deb44d463febf80579b32a3b3b7c7f
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-10-01 23:57:42 +0300

    Home component
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

---
+++ Client/src/styled-components/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <h1> This is the Homee page </h1>
+      <p> Hey there this is a home component</p>
+    </>
+  )
+}
+
+export default Home
