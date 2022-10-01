fb1a99eb94dd4b9cf3631e48ba1fd0c9ff70866c
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-10-01 23:59:15 +0300

    about us page
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

---
+++ Client/src/styled-components/Pages/aboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <h1> This is the about us page </h1>
+      <p> Hey there this is a about component</p>
+    </>
+  )
+}
+
+export default Home
