24b83b95eaaa02314f53acc9e7e83b1fff0c9267
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-10-02 00:00:34 +0300

    more
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- Client/src/styled-components/Pages/aboutUs.js
+++ Client/src/styled-components/Pages/aboutUs.js
@@ -4,7 +4,14 @@ function Home() {
   return (
     <>
       <h1> This is the about us page </h1>
+          <p> Hey there this is a about component</p>
       <p> Hey there this is a about component</p>
+      <p> Hey there this is a about</p>
+      <p> Hey there this is a about component</p>
+      <p> Hey there this is a about component</p>
+      <p> Hey there this is a about component</p>
+      <p> Hey there this is a about component</p>
+          
     </>
   )
 }
